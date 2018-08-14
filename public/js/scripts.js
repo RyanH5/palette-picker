cf5d9c8d98602f21a18f563d4419e952c6296a08
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-08-13 22:53:56 -0600

    Remove redundant hex generating

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,24 +1,29 @@
 const paletteGeneratorBtn = document.getElementById("palette--generator-btn");
+const savePaletteBtn = document.getElementById("palette--save-btn");
 
 const generateRandomColor = () => {
-  var hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
+  var hexValues = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E", "F"];
   var newColor = "#";
   
   for ( let i = 0; i < 6; i++ ) {
-    let x = Math.round( Math.random() * 14 );
+    let x = Math.round( Math.random() * 15 );
     let y = hexValues[x];
     newColor += y;
   }
-  console.log(newColor)
+
+  // return newColor;
   document.getElementById("first--color-possibility").style.backgroundColor = newColor;
   document.getElementById("first--color-hex").innerHTML = newColor;
 }
 
 const createColorPalette = () => {
+  const colorList = [];
   for(var i = 1; i < 6; i++) {
-    generateRandomColor();
-    console.log(newColor)
+    let newColor = generateRandomColor();
+    colorList.push(newColor);
     }
+  console.log(colorList)
 }
+
 
 paletteGeneratorBtn.addEventListener("click", createColorPalette);
