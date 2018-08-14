c957ebe4d87f3f0187b6e3c20a2823e796ae25c3
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-08-13 22:16:15 -0600

    Randomly generate color for first color splotch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,23 @@
 const paletteGeneratorBtn = document.getElementById("palette--generator-btn");
 
 const generateRandomColor = () => {
-  console.log('yo')
+  var hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
+  var newColor = "#";
+  
+  for ( let i = 0; i < 6; i++ ) {
+    let x = Math.round( Math.random() * 14 );
+    let y = hexValues[x];
+    newColor += y;
+  }
+  console.log(newColor)
+  document.getElementById("first--color-possibility").style.backgroundColor = newColor;
+  document.getElementById("first--color-hex").innerHTML = newColor;
 }
 
 const createColorPalette = () => {
-  for(var i = 0; i < 5; i++) {
+  for(var i = 1; i < 6; i++) {
     generateRandomColor();
-    console.log('mama')
+    console.log(newColor)
     }
 }
 
