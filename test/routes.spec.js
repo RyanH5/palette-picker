1377480bfb4c51bf9e6c9d0468f312dde56ab757
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-08-17 08:56:47 -0600

    Pass get/post projects tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,13 +1,38 @@
+var assert = require('assert');
 const chai = require('chai');
 const should = chai.should();
+const expect = chai.expect();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
+chai.use(chaiHttp)
+
+describe('GET /api/v1/projects', () => {
+  it('should return all projects', done => {
+    chai.request(server)
+      .get('/api/v1/projects')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.should.have.lengthOf(11)
+        done();
+      }
+      )});
+});
+
+describe('POST /api/v1/projects', () => {
+  it('should create a new project', done => {
+    chai.request(server)
+      .post('/api/v1/projects')
+      .send({
+        'name': 'Seminoles colors'
+      })
+      .end((err, response) => {
+        response.should.have.status(201);
+        // response.body.should.be.json;
+        response.body.should.be.a('object');
+        done();
+      });
   });
 });
